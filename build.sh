2ef6e9f2604a29462ab9b8d17a2f84fed803eb4c
Alex Good <alex@memoryandthought.me>
2023-01-10 11:13:03 +0000

    Fix broken cross-references

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-asciidoctor -r asciidoctor-diagram --backend html src/index.adoc --destination-dir ./build/
+asciidoctor -v -r asciidoctor-diagram --backend html src/index.adoc --destination-dir ./build/
